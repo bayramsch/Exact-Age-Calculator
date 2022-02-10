b8a3d4f3161e609db0571c921f03d0b174b34f25
Bayramsch <bayramkmz7240@gmail.com>
2022-02-10 15:43:12 +0100

    change

--- App1.js
+++ App1.js
@@ -52,32 +52,3 @@ birthdayNameByName.addEventListener("change", ()=>{
 })
 
 
-
-
-
-if(new Date().getDate() - new Date(birthdayNameByName.value).getDate()<0) {
-    dayBySelectorName.innerText = new Date().getDate() - new Date(birthdayNameByName.value).getDate() + 30;
-    monthsBySelectorName.innerText -= 1;
-} else {
-    dayBySelectorName.innerText = new Date().getDate() - new Date(birthdayNameByName.value).getDate(); 
-}
-
-
- 
-if (new Date().getMonth() - new Date(birthdayNameByName.value).getMonth() < 0){
-    monthsBySelectorName.innerText =new Date().getMonth() - new Date(birthdayNameByName.value).getMonth() + 12;
-    yearByClassName.innerText -= 1;
-} else { 
-    monthsBySelectorName.innerText = new Date().getMonth() - new Date(birthdayNameByName.value).getMonth();
-}
-
-yearByClassName.innerText = new Date().getFullYear() - new Date(birthdayNameByName.value).getFullYear();
-
-let hours = new Date().getHours() - new Date(birthdayNameByName.value).getHours();
-hoursByIdName.innerText = hours;
-
-let minutes = new Date().getMinutes() - new Date(birthdayNameByName.value).getMinutes();
-minutesByClassName.innerText = minutes;
-
-let seconds = new Date().getSeconds() - new Date(birthdayNameByName.value).getSeconds();
-secondsByClassName.innerText = seconds;
